1cde356f9e03d5af99512d0557c3928def4be03b
EnzoMagurno <magurnoenzo31602@gmail.com>
2023-06-16 01:12:10 -0300

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import Image from 'next/image';
 import React from 'react';
 import { Josefin_Sans, Roboto } from 'next/font/google';
+import NavBarFooter from '@/components/navBarFooter/NavBarFooter';
 
 const josefin = Josefin_Sans({
   weight: ['400'],
@@ -15,7 +16,7 @@ const roboto = Roboto({
 
 const Home = () => {
   return (
-    <div className='p-5'>
+    <div className='p-5 h-screen box-border'>
       <div className='pt-2 pb-2 mt-12'>
         <h4 className={`${josefin.className}  text-gray-500`}>Hello, User</h4>
         <h3 className={`${roboto.className} text-3xl`}>Where We Go?</h3>
@@ -25,6 +26,7 @@ const Home = () => {
         placeholder='Search'
         className={`w-full h-12 rounded-full shadow-input outline-none pl-3 mt-2 mb-2`}
       />
+      <NavBarFooter />
     </div>
   );
 };
