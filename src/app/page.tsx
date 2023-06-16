f764ddc071ffcc319f95a65630383796fbbb249d
EnzoMagurno <magurnoenzo31602@gmail.com>
2023-06-15 22:12:11 -0300

    NavBarFooter styled added to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,31 +2,33 @@
 import Image from 'next/image';
 import React from 'react';
 import { Josefin_Sans, Roboto } from 'next/font/google';
+import NavBarFooter from '@/components/navBarFooter/NavBarFooter';
 
 const josefin = Josefin_Sans({
-	weight: ['400'],
-	subsets: ['latin'],
+  weight: ['400'],
+  subsets: ['latin'],
 });
 
 const roboto = Roboto({
-	weight: '700',
-	subsets: ['cyrillic'],
+  weight: '700',
+  subsets: ['cyrillic'],
 });
 
 const Home = () => {
-	return (
-		<div className='p-5'>
-			<div className='pt-2 pb-2 mt-12'>
-				<h4 className={`${josefin.className}  text-gray-500`}>Hello, User</h4>
-				<h3 className={`${roboto.className} text-3xl`}>Where We Go?</h3>
-			</div>
+  return (
+    <div className='p-5 h-screen box-border'>
+      <div className='pt-2 pb-2 mt-12'>
+        <h4 className={`${josefin.className}  text-gray-500`}>Hello, User</h4>
+        <h3 className={`${roboto.className} text-3xl`}>Where We Go?</h3>
+      </div>
       <input
-				type='text'
+        type='text'
         placeholder='Search'
-				className={`w-full h-12 rounded-full shadow-input outline-none pl-3 mt-2 mb-2`}
-			/>
-		</div>
-	);
+        className={`w-full h-12 rounded-full shadow-input outline-none pl-3 mt-2 mb-2`}
+      />
+      <NavBarFooter />
+    </div>
+  );
 };
 
 export default Home;
