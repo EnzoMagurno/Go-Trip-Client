b114323c3d7692f1bc41df9f6297eaf85aa937a7
Adiel <wereber.dragons14@live.com.mx>
2023-07-03 12:23:56 -0600

    usersAdmin Page

--- src/components/AdminContainers/UsersAdminContainer/ActiveUsers.tsx
+++ src/components/AdminContainers/UsersAdminContainer/ActiveUsers.tsx
@@ -1,16 +1,16 @@
 'use client';
 import { FaUserEdit } from 'react-icons/fa';
-import { useState } from 'react';
-import { updatingUsersReal } from '../../redux/Features/UsersReal/usersRealSlice';
+import { useState, useEffect } from 'react';
+import { updatingUsersReal, deleteUsersReal, fetchingUsersReal, activeUsersReal, getallUsersReal } from '../../../redux/Features/UsersReal/usersRealSlice';
 import { useDispatch } from 'react-redux';
-import SelectUniversal from '../SelectUniversal/SelectUniversal';
-
+import SelectUniversal from '../../SelectUniversal/SelectUniversal';
 
 
 
 const UsersAdminContainer = (props) => {
 	const dispatch = useDispatch();
 	const {
+		deletedAt,
 		id,
 		name,
 		photoUser,
@@ -31,60 +31,43 @@ const UsersAdminContainer = (props) => {
 		thirdPartyCreated,
 	} = props;
 
+
+
+
 	const [userDataEdit, setUserEdit] = useState({
+		deletedAt,
+		id,
+		name,
 		photoUser,
 		postalCode,
 		birthday,
 		country,
+		phone,
 		phoneCode,
 		gender,
+		email,
 		password,
 		confirmPassword,
+		address,
 		dniPasaport,
 		status,
+		rol,
 		dniPassport,
 		thirdPartyCreated,
-		id: id,
-		name: name,
-		phone: phone,
-		email: email,
-		address: address,
-		rol: rol,
 	});
 
-	const [editUserWindow, setEditUserWindow] = useState('hidden');
-
-	const showFormEditUser = () => {
-		setUserEdit({
-			photoUser,
-			postalCode,
-			birthday,
-			country,
-			phoneCode,
-			gender,
-			password,
-			confirmPassword,
-			dniPasaport,
-			status,
-			dniPassport,
-			thirdPartyCreated,
-			id: id,
-			name: name,
-			phone: phone,
-			email: email,
-			address: address,
-			rol: rol,
-		});
-
-		if (editUserWindow) {
-			setEditUserWindow('');
-		} else {
-			setEditUserWindow('hidden');
-		}
-	};
 
 
-	const Rol = rol[0].toUpperCase() + rol.slice(1, rol.length)
+
+
+
+
+
+	const Rol =  rol ? rol[0].toUpperCase() + rol.slice(1, rol.length) : ""
+	
+
+
+	
 
 	const handleUserChanges = (e) => {
 		const name = e.target.name;
@@ -96,20 +79,44 @@ const UsersAdminContainer = (props) => {
 		});
 	};
 
-	const updateUser = (e) => {
+
+
+
+
+	const deletUser = (e) => {
+		e.preventDefault()
+		dispatch(deleteUsersReal(id))
+		dispatch(getallUsersReal())
+		dispatch(fetchingUsersReal())
+	
+		
+	}
+	/* const updateUser = (e) => {
 		e.preventDefault();
 
 		dispatch(updatingUsersReal(userDataEdit));
-	};
+	}; */
 
-	const [userIsActive, setUserIsActive] = useState('Lock user');
-	const handleCheckChange = () => {
-		if (status) {
+	const [userIsActive, setUserIsActive] = useState(deletedAt ? "Active user" : "Lock user");
+	const handleCheckChange =  () => {
+		
+		if (!deletedAt) {
+
 			setUserIsActive('Active user');
+			dispatch(deleteUsersReal(id))
+			dispatch(getallUsersReal())
+		dispatch(fetchingUsersReal())
+	
 		} else {
 			setUserIsActive('Lock user');
+			 dispatch(activeUsersReal(id))
+			 dispatch(getallUsersReal())
+		dispatch(fetchingUsersReal())
+			
 		}
 	};
+
+
 
 	
 
@@ -126,19 +133,21 @@ const UsersAdminContainer = (props) => {
 
 				<h5 className='text-center pt-3 pb-3 font-bold  text-2xl'>{name}</h5>
 
-				<div className='grid grid-cols-2 gap-2   bg-red-100 '>
+				<div className='grid grid-cols-2 gap-2 mb-3'>
 					<div>
 						<h5 className='text-sm font-medium'>Country</h5>
 						<p className='text-sm'>{country}</p>
 					</div>
-					<div className='w-full '>
-						<h5 className='text-sm font-medium '>Email</h5>
-						<p className='text-sm'>{email}</p>
-					</div>
+
 					<div>
 						<h5 className='text-sm font-medium '>Phone</h5>
 						<p className='text-sm'>{phone}</p>
 					</div>
+					<div className='overflow-auto' >
+						<h5 className='text-sm font-medium '>Email</h5>
+					<span className='text-sm  inline-block  '>{email}</span>
+					</div>
+					
 					<div>
 						<h5 className='text-sm font-medium'>Rol</h5>
 						<p className='text-sm'>{Rol}</p>
@@ -153,7 +162,7 @@ const UsersAdminContainer = (props) => {
 						Rol
 					</label>
 					<div className='relative  w-full h-full '>
-					<SelectUniversal optionDefault={Rol} />
+					<SelectUniversal optionDefault={Rol} data={userDataEdit} />
 					
 					</div>
 					
@@ -178,7 +187,8 @@ const UsersAdminContainer = (props) => {
 							{userIsActive}
 						</label>
 						<div className='flex items-center w-full h-10'>
-							<label
+
+									<label
 								htmlFor={id}
 								className=' shadow-inset_custom w-14 h-8 cursor-pointer rounded-full inline-block relative dark:shadow-inset_BlueSky '
 								onChange={handleCheckChange}
@@ -186,10 +196,14 @@ const UsersAdminContainer = (props) => {
 								<input type='checkbox' id={id} className=' sr-only peer' />
 								<span className='w-6 h-6 top-1 peer-checked:left-1 peer-checked:bg-zinc-600 absolute rounded-full bg-green-600   left-7 transition-all duration-300'></span>
 							</label>
+					
+			
+							
 						</div>
 					</div>
+					
 
-					<div className='flex items-center mt-2 mb-2 h-10'>
+							<div className='flex items-center mt-2 mb-2 h-10'>
 						<label
 					
 							className=' font-medium text-zinc-500 w-2/5 text-sm'
@@ -197,19 +211,17 @@ const UsersAdminContainer = (props) => {
 							Delete User
 						</label>
 						<button
-							onClick={updateUser}
+							onClick={deletUser}
 							className='w-full bg-red-700 text-white p-2 rounded-md'
 						>
 							Delete User
 						</button>
 					</div>
+
+
+					
 				</div>
-				<button
-					onClick={updateUser}
-					className='w-full mt-5 bg-iconsPurple text-white p-2 rounded-md'
-				>
-					Save changes
-				</button>
+		
 			</form>
 		</div>
 	);
--- src/components/OptionsUniversal/OptionsUniversal.tsx
+++ src/components/OptionsUniversal/OptionsUniversal.tsx
@@ -1,12 +1,17 @@
 
-
 const OptionsUniversal = ({ value, name, selectOptions, optionSelected, showOptions }) => {
 
+    
 
 
 
     return (
-        <li onClick={() => { selectOptions(name, value); showOptions()}} className={`rounded-md list-none pl-2 h-8 flex items-center cursor-pointer transition duration-200 hover:bg-iconsPurple hover:text-white ${optionSelected === name ? "border-2 border-orangeBg" : "" }`}>{name}</li>
+        <li onClick={() => { 
+            selectOptions(name, value); 
+            showOptions(); 
+           
+        
+        }} className={`rounded-md list-none pl-2 h-8 flex items-center cursor-pointer transition duration-200 hover:bg-iconsPurple hover:text-white ${optionSelected === name ? "border-2 border-orangeBg" : "" }`}>{name}</li>
     )
 };
 
